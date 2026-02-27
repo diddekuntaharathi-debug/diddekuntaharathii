44c08a4930a204cdb58881fb1092e2777214a17e
DIDDEKUNTA HARATHI <diddekuntaharathi@gmail.com>
2026-02-27 11:07:39 +0530

    Add files via upload

---
+++ def program.py
@@ -0,0 +1,13 @@
+def usercheck(choice):
+    if(choice==1):
+        print("Admin")
+    elif(choice==2):
+        print("guest")
+    elif(choice==3):
+        print("Adminstarator")
+    else:
+        print("enter valid choice")
+usercheck(1)
+usercheck(2)
+usercheck(3)
+usercheck(5)
---
+++ type conversions.py
@@ -0,0 +1,40 @@
+Python 3.13.12 (tags/v3.13.12:1cbe481, Feb  3 2026, 18:22:25) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=4
+>>> b=4.6
+>>> c=a+b
+>>> print(type(a))
+<class 'int'>
+>>> print(type(b))
+<class 'float'>
+>>> a="3"
+>>> print(a)
+3
+>>> print(type(a))
+<class 'str'>
+>>> b=6
+>>> a=int(a)
+>>> print(a)
+3
+>>> sum=a*a
+>>> print(sum)
+9
+>>> print(type(a))
+<class 'int'>
+>>> a=10
+>>> b=6.7
+>>> c=5+7i
+SyntaxError: invalid decimal literal
+>>> print(a)
+10
+>>> print(type(a))
+<class 'int'>
+>>> print(b)
+6.7
+>>> print(type(b))
+<class 'float'>
+>>> c=2+9j
+>>> print(c)
+(2+9j)
+>>> print(type(c))
+<class 'complex'>
