afbd1ea3575189c6bc909417e43b911c73439be6
DIDDEKUNTA HARATHI <diddekuntaharathi@gmail.com>
2026-03-05 11:56:52 +0530

    Add files via upload

---
+++ STRIP FUNCTIONS.py
@@ -0,0 +1,8 @@
+#strip function
+T="   pYThon   "
+print(T.upper())
+print(T.lstrip())
+print(T.rstrip())
+print(len(T))
+print(T.find('T'))
+print(T.replace("pY","java"))
---
+++ SWITCH CASE USING DAYS.py
@@ -0,0 +1,20 @@
+#SWITCH CASE
+day=int(input("Enter a num:"))
+match day:
+  case 1:
+      print("monday")
+  case 2:
+      print("Tuesday")
+  case 3:
+      print("Wednesday")
+  case 4:
+      print("Thrusday")
+  case 5:
+      print("Friday")
+  case 6:
+      print("Saturday")
+  case 7:
+      print("sunday")
+  case _:
+      print("Invalid")
+    
