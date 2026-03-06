93ef8439f6663c1ff0f429810599b02e677d82f4
DIDDEKUNTA HARATHI <diddekuntaharathi@gmail.com>
2026-03-06 11:31:01 +0530

    Add files via upload

---
+++ FIBONNIC SERIES.py
@@ -0,0 +1,11 @@
+#FIBONNIC SERIES
+n=int(input("enter n value:"))
+a=0
+b=1
+for i in range(0,n):
+
+    c=a+b
+    a=b
+    b=c
+    print(c,end="")
+    
---
+++ dict.py
@@ -0,0 +1,4 @@
+dict={ 'tag':{1:"boys"},'lucky':{2:"girl"}}
+print(dict)
+print(dict['tag'])
+print(dict['lucky'])
